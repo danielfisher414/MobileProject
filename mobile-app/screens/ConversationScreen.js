101d7f4ca7729a8739ee8f4f627bdebb21020421
Daniel <47793260+danielfisher414@users.noreply.github.com>
2023-03-12 23:09:29 +0000

    the convsation chats screen can now recieve messages and is able to send messages

--- mobile-app/screens/ConversationScreen.js
+++ mobile-app/screens/ConversationScreen.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View} from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 import { GiftedChat, Bubble, Avatar } from 'react-native-gifted-chat';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class ConversationScreen extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.getChatInfo();
+    this.refreshInterval = setInterval(this.getAllConversations, 1000); // refresh every 5 seconds
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.refreshInterval);
   }
   constructor(props) {
     super(props);
     this.state = {
       messages: [], // array of message objects
       inputText: '', // text in input box
-      session_token:'',
-      chat_id:'',
-      chat_name:'',
-      author_id:'',
-      showAvatar:'',
+      session_token: '',
+      chat_id: '',
+      chat_name: '',
+      author_ids: null,
+      showAvatar: '',
     };
   }
 
@@ -25,12 +30,13 @@ class ConversationScreen extends Component {
       AsyncStorage.getItem('chat_id'),
       AsyncStorage.getItem('chat_name'),
       AsyncStorage.getItem('session_token'),
+      AsyncStorage.getItem('user_id'),
     ])
-      .then(([chat_id, chat_name,session_token ]) => {
+      .then(([chat_id, chat_name, session_token, user_id]) => {
         if (chat_id && chat_name) {
-          this.setState({ chat_id, chat_name,session_token });
-          console.log(this.state.chat_id,this.state.chat_name);
-          console.log(this.state.session_token);
+          this.setState({ chat_id, chat_name, session_token, user_id });
+          // console.log(this.state.chat_id,this.state.chat_name);
+          // console.log(this.state.session_token);
           this.getAllConversations();
         } else {
           // handle missing values
@@ -40,7 +46,8 @@ class ConversationScreen extends Component {
       .catch((error) => console.log(error));
   };
 
-  getAllConversations = () => {
+  getAllConversations = () => { // need to find a way to do this on effect due to needing to refresh the app to show
+    // newer chat data
     fetch('http://localhost:3333/api/1.0.0/chat/' + this.state.chat_id, {
       method: 'GET',
       headers: {
@@ -59,35 +66,26 @@ class ConversationScreen extends Component {
         }
       })
       .then((data) => {
-        
-      const messages = data.messages.map(message => ({
-      
-        _id: message.message_id,
-        text: message.message,
-        createdAt: new Date(message.timestamp),
-        
-        user: {
-          
-          id: message.author.user_id,
-          name: message.author.first_name +" "+message.author.last_name,
-            
-        },
-        
-      }));
-      
-      // this.setState({})
-      // alert("hello")
-      // console.log("first test: "+this.state.messages[0].user.author_id)
-      
-      // console.log(messages);
-      messages.forEach(message => {
-        console.log("element: "+message.user.author_id);
-        this.setState({ author_id: message.user.author_id});  
-      });
-      this.setState({ messages });
-      
+        console.log(data);
+        const messages = data.messages.map(message => ({
 
-      // console.log("im here: "+this.state.messages[0].user.author_id);
+          _id: message.message_id,
+          text: message.message,
+          createdAt: new Date(message.timestamp),
+
+          user: {
+
+            _id: message.author.user_id,
+            name: message.author.first_name + " " + message.author.last_name,
+
+          },
+
+        }));
+
+        const authorIds = messages.map((message) => message.user.id);
+
+        this.setState({ messages, author_ids: authorIds });
+
       })
       .catch((error) => {
         console.error(error.message); // Handle the error
@@ -96,119 +94,156 @@ class ConversationScreen extends Component {
 
 
   onSend = (newMessages) => {
+
+    // console.log(JSON.stringify(newMessages));
+    // console.log(newMessages[0].text);
+    const messageSent = newMessages[0].text;
+    // console.log("chatmsg "+newMessages[0]);
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, newMessages),
-    }));
-  };
+    }), () => {
+      // Make network request to post new message
+      const lastMessage = newMessages[0];
+      const { text, user, createdAt } = lastMessage;
+      const data = { text, user, createdAt };
+      
+      
+      fetch('http://localhost:3333/api/1.0.0/chat/'+this.state.chat_id+'/message', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'X-Authorization': this.state.session_token
+        },
+        body: JSON.stringify({
+          message: messageSent,
+        })
+        
+      });
+      this.getAllConversations();
 
+    });
+  };
+  
+  // renderBubble = props => {
+  //   return (
+  //     <Bubble
+  //       {...props}
+  //       wrapperStyle={{
+  //         left: {
+  //           backgroundColor: "#f0f0f0"
+  //         }
+  //       }}
+  //     />
+  //   );
+  // };
+  
   renderAvatar=(props)=> {
-    if(this.state.author_id==this.state.user_id){
+    const { currentMessage } = props;
+    const authorId = currentMessage.user._id;
+    console.log("im here hello "+authorId);
+    if(authorId==this.state.user_id){
       return null;
       
     }else{
       return (
         <Avatar
           {...props}
-          hide={'left'}
-          
+          position= "left"
+          style={{ marginLeft: 8, marginRight: 8 }}
         />
       );
     };
   };
 
-  renderBubble=(props)=> {
-    // alert(this.state.author_id);
-    if(this.state.author_id==this.state.user_id){
-      
-    return (
-      <View
-      style={{
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-      }}
-      >
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          left: {
-            backgroundColor: '#cccccc',
-          },
-        }}
-        textStyle={{
-          left: {
-            color: 'black',
-          },
-        }}
-        position={'right'} //moves the position of the bubble
-        renderAvatar={() => null}
-      />
-      </View>
-    );
-      }else{
-        return (
-          <Bubble
-            {...props}
-            wrapperStyle={{
-              left: {
-                backgroundColor: '#cccccc',
-              },
-            }}
-            textStyle={{
-              left: {
-                color: 'black',
-              },
-            }}
-            position={'left'} //moves the position of the bubble
-          />
-        );
-      }
+  renderBubble = props => {
+    const { currentMessage } = props;
+    const authorId = currentMessage.user._id;
+
+    console.log(authorId);
+    console.log("current user id: " + this.state.user_id);
+    if (authorId == this.state.user_id) {
+
+      return (
+
+
+        <Bubble
+          {...props}
+          // wrapperStyle={{
+          //   left: {
+          //     backgroundColor: '#cccccc',
+          //   },
+          // }}
+          // textStyle={{
+          //   left: {
+          //     color: 'black',
+          //   },
+          // }}
+          position={'right'} //moves the position of the bubble
+          // renderAvatar={() => null}
+          style={{ marginRight: 60, marginBottom: 10 }} // add marginRight style
+        />
+
+      );
+    } else {
+
+      return (
+
+        <Bubble
+          {...props}
+          
+          // wrapperStyle={{
+          //   left: {
+          //     backgroundColor: '#cccccc',
+          //   },
+          // }}
+          // textStyle={{
+          //   left: {
+          //     color: 'black',
+          //   },
+          // }}
+          position={'left'} //moves the position of the bubble
+          style={{ marginRight: 60, marginBottom: 10 }} // add marginRight style
+        />
+
+      );
+    }
   }
 
+  
+
   render() {
-    console.log("here-> :"+this.state.author_id);
-    
-    if(this.state.author_id==this.state.user_id){
-      return (
-      
-        <View style={{ backgroundColor: "white", flex: 1 }}>
-          {/* {alert(this.state.chat_id)} */}
+    // console.log("here-> :"+this.state.author_id);
+
+
+    console.log("hi2");
+    return (
+
+      <View style={{ backgroundColor: "white", flex: 1 }}>
+        {/* {alert(this.state.chat_id)} */}
+
         <GiftedChat
+
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
           
           // user={{
-          //   _id: 1,
-          // }}
-          // {_id ==this.chat_id}
-          renderBubble={this.renderBubble} // bubble render
-        
-          renderAvatar={null} // avatar comment this function for it to show
-          
-  
-        />
-        </View>
-      );
-    }
-    return (
-      
-      <View style={{ backgroundColor: "white", flex: 1 }}>
-        {/* {alert(this.state.chat_id)} */}
-      <GiftedChat
-        messages={this.state.messages}
-        onSend={messages => this.onSend(messages)}
-        
-        // user={{
-        //   _id: 1,
-        // }}
-        // {_id ==this.chat_id}
-        renderBubble={this.renderBubble} // bubble render
-      
-        renderAvatar={null} // avatar comment this function for it to show
-        
 
-      />
+          //     // id: message.author.user_id,
+          //     name: message.author.first_name + " " + message.author.last_name,
+
+          // }}
+
+          renderBubble={this.renderBubble} // bubble render
+          renderAvatar={this.renderAvatar}
+          // showUserAvatar={true}
+          // renderMessage={this.renderMessage}
+
+
+        />
       </View>
     );
+
   }
 }
 
--- mobile-app/screens/editProfile.js
+++ mobile-app/screens/editProfile.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View, Button, Alert, StyleSheet, Image,TouchableOpacity  } from 'react-native';
+import { Text, TextInput, View, Button, Alert, Modal, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Input } from 'react-chat-elements';
-
+import passwordValidator from 'password-validator';
 class EditProfile extends Component {
     componentDidMount() {
         this.getUserInfo();
@@ -14,9 +14,13 @@ class EditProfile extends Component {
             first_name: '',
             last_name: '',
             email: '',
-            password:'',
+            password: '',
             user_id: '',
             session_token: '',
+            visible: false,
+            testOriginalPassword: '',
+            newPassword: '',
+            newPassword2: '',
         };
         this.getUserInfo = this.getUserInfo.bind(this);
     }
@@ -64,39 +68,90 @@ class EditProfile extends Component {
                 first_name: this.state.first_name,
                 last_name: this.state.last_name,
                 email: this.state.email,
-                // password: this.state.password,
-              }),
+            }),
         })
-        .then(response => {
-            if (response.status === 200) {
-                // Success
-                if (response.headers.get("Content-Type").indexOf("application/json") !== -1) {
-                    return response.json(); // Parse response body as JSON
+            .then(response => {
+                if (response.status === 200) {
+                    // Success
+                    if (response.headers.get("Content-Type").indexOf("application/json") !== -1) {
+                        return response.json(); // Parse response body as JSON
+                    } else {
+                        return response.text(); // Return response body as plain text
+                    }
                 } else {
-                    return response.text(); // Return response body as plain text
+                    // Error
+                    throw new Error('Something went wrong');
                 }
-            } else {
-                // Error
-                throw new Error('Something went wrong');
-            }
-        })
-        .then(data => {
-            if (typeof data === "object") {
-                console.log(data); // Handle the JSON response
-                this.setState({
-                    first_name: data.first_name,
-                    last_name: data.last_name,
-                    email: data.email,
-                    
+            })
+            .then(data => {
+                if (typeof data === "object") {
+                    console.log(data); // Handle the JSON response
+                    this.setState({
+                        first_name: data.first_name,
+                        last_name: data.last_name,
+                        email: data.email,
+                        
+                    });
+
+                } else {
+                    console.log(data); // Handle the plain text response
+                }
+            })
+            .catch(error => {
+                console.error(error); // Handle the error
+            });
+    };
+    handleUpdatePassword = () => {
+        
+        if(this.state.newPassword==this.state.newPassword2){
+
+            if(this.validatePassword(this.state.newPassword)==true){
+            
+            this.setState({password:this.state.newPassword});
+
+            fetch('http://localhost:3333/api/1.0.0/user/' + this.state.user_id, {
+                method: 'PATCH',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-Authorization': this.state.session_token,
+                },
+                body: JSON.stringify({
+                    password: this.state.password
+                }),
+            })
+                .then(response => {
+                    if (response.status === 200) {
+                        // Success
+                        if (response.headers.get("Content-Type").indexOf("application/json") !== -1) {
+                            return response.json(); // Parse response body as JSON
+                        } else {
+                            return response.text(); // Return response body as plain text
+                        }
+                    } else {
+                        // Error
+                        throw new Error('Something went wrong');
+                    }
+                })
+                .then(data => {
+                    if (typeof data === "object") {
+                        console.log(data); // Handle the JSON response
+                        this.setState({
+                            password: data.password
+                            
+                        });
+    
+                    } else {
+                        console.log(data); // Handle the plain text response
+                    }
+                })
+                .catch(error => {
+                    console.error(error); // Handle the error
                 });
-                
-            } else {
-                console.log(data); // Handle the plain text response
+
             }
-        })
-        .catch(error => {
-            console.error(error); // Handle the error
-        });
+        }
+
     };
 
     getUserInfo = () => {
@@ -116,66 +171,151 @@ class EditProfile extends Component {
             .catch((error) => console.log(error));
     };
 
-    goBack = () =>{
+    goBack = () => {
         const { navigation } = this.props;
         navigation.navigate('Profile');
-      };
-
-      handleFirstNameTextChange=(newtext)=>{
-        this.setState({first_name:newtext})
-      };
-
-    handleLastNameTextChange=(newtext)=>{
-        this.setState({last_name:newtext})
-      };
-
-    handleEmailTextChange=(newtext)=>{
-        this.setState({email:newtext})
     };
 
-    handlePasswordTextChange=(newtext)=>{
-        this.setState({password:newtext})
+    handleFirstNameTextChange = (newtext) => {
+        this.setState({ first_name: newtext })
     };
+
+    handleLastNameTextChange = (newtext) => {
+        this.setState({ last_name: newtext })
+    };
+
+    handleEmailTextChange = (newtext) => {
+        this.setState({ email: newtext })
+    };
+
+    handlePasswordTextChange = (newtext) => {
+        this.setState({ password: newtext })
+    };
+    handleOverlay = () => {
+        this.setState({ visible: !this.state.visible });
+    };
+
+    handleFirstNameTextChange = (newtext) => {
+        this.setState({ first_name: newtext })
+    };
+
+    handleFirstNewPassword = (newtext) => {
+        this.setState({ newPassword: newtext })
+    };
+    
+    handle2ndNewPassword = (newtext) => {
+        this.setState({ newPassword2: newtext })
+    };
+
+    handleOriginalPassword = (newtext) => {
+        this.setState({ testOriginalPassword: newtext })
+    };
+
+    handleChangePassword=()=>{
+        // console.log(this.state.testOriginalPassword);
+        // console.log(this.state.password);
+        // if(this.state.testOriginalPassword==this.state.password){
+            console.log(2);
+            if(this.state.newPassword==this.state.newPassword2){
+                console.log(3);
+                if(this.validatePassword(this.state.newPassword)==true){
+                    console.log(4);
+                this.setState({password:this.state.newPassword});
+                this.handleUpdatePassword;
+                console.log(5);
+                }
+            }
+        // }
+    };
+
+    validatePassword = (password) => {
+        const schema = new passwordValidator();
+    
+        schema
+          .is().min(8)  // Minimum length 8
+          .is().max(100)  // Maximum length 100
+          .has().uppercase()  // Must have uppercase letters
+          .has().lowercase()  // Must have lowercase letters
+          .has().digits()  // Must have digits
+          .has().symbols()
+          .has().not().spaces()  // Should not have spaces
+          .is().not().oneOf(['Passw0rd', 'Password123']);  // Blacklist these values
+    
+        if (schema.validate(password)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
 
     render() {
         return (
             <View style={styles.container}>
-                
+
                 {/* <Text style={styles.headerName}>Edit Profile</Text> */}
-                
+
                 <Image
                     style={styles.profilePic}
                     source={{ uri: 'https://www.ateneo.edu/sites/default/files/styles/large/public/2021-11/istockphoto-517998264-612x612.jpeg?itok=aMC1MRHJ' }}
                 />
                 <Text style={styles.name}>{this.state.first_name} {this.state.last_name}</Text>
                 <View style={styles.changeProfileInputs}>
-                <Text style={styles.btnTitle}>Title</Text>
+                    <Text style={styles.btnTitle}>Title</Text>
                     <TextInput style={styles.editProfileInputs}
-                     multiline={true} onChangeText={this.onChangeText} value={this.state.text}
-                     placeholder='Drag Image or img link'
+                        multiline={true} onChangeText={this.onChangeText} value={this.state.text}
+                        placeholder='Drag Image or img link'
                     />
                     <Text style={styles.btnTitle}>First Name</Text>
-                    <TextInput style={styles.editProfileInputs} placeholder='first name' 
-                                onChangeText={this.handleFirstNameTextChange}
-                                value={this.state.first_name}/>
-<Text style={styles.btnTitle}>Last Name</Text>
-                    <TextInput style={styles.editProfileInputs} placeholder='last name' 
-                     onChangeText={this.handleFirstNameTextChange}
-                     value={this.state.last_name}/>
-                     <Text style={styles.btnTitle}>Email</Text>
-                    <TextInput style={styles.editProfileInputs} placeholder='email' 
-                    onChangeText={this.handleFirstNameTextChange}
-                    value={this.state.email}/>
+                    <TextInput style={styles.editProfileInputs} placeholder='first name'
+                        onChangeText={this.handleFirstNameTextChange}
+                        value={this.state.first_name} />
+                    <Text style={styles.btnTitle}>Last Name</Text>
+                    <TextInput style={styles.editProfileInputs} placeholder='last name'
+                        onChangeText={this.handleFirstNameTextChange}
+                        value={this.state.last_name} />
+                    <Text style={styles.btnTitle}>Email</Text>
+                    <TextInput style={styles.editProfileInputs} placeholder='email'
+                        onChangeText={this.handleFirstNameTextChange}
+                        value={this.state.email} />
                     {/* <Text style={styles.btnTitle}>Password</Text>
                     <TextInput style={styles.editProfileInputs} placeholder='password' 
                     onChangeText={this.handleFirstNameTextChange}
                     value={this.state.password}/> */}
-                    
-                    <Button title="Change Profile" onPress={this.handleUpdateProfile}/>
-                    <View style={{marginTop:10}}>
-                    <Button title="Go Back" onPress={this.goBack}/>
+                    <Button title='Change Password' onPress={this.handleOverlay} />
+                    <Button title="Change Profile" onPress={this.handleUpdateProfile} />
+                    <View style={{ marginTop: 10 }}>
+                        <Button title="Go Back" onPress={this.goBack} />
                     </View>
                 </View>
+
+                {/* OVERLAY */}
+                <Modal animationType="fade" transparent={true} visible={this.state.visible}>
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+                        <View style={{ backgroundColor: 'white', padding: 60 }}>
+                            <Text style={styles.btnTitle}>Type Original Password</Text>
+
+                            {/* <TextInput style={styles.editProfileInputs} 
+                                onChangeText={this.handleOriginalPassword}
+                                value={this.state.testOriginalPassword} /> */}
+
+                            <Text style={styles.btnTitle}>Type New Password</Text>
+                            <TextInput style={styles.editProfileInputs}
+                                onChangeText={this.handleFirstNewPassword}
+                                value={this.state.newPassword} />
+
+                            <Text style={styles.btnTitle}>Verify Type New Password</Text>
+                            <TextInput style={styles.editProfileInputs}
+                                onChangeText={this.handle2ndNewPassword}
+                                value={this.state.newPassword2} />
+
+                            <Button title="Change Password" onPress={this.handleUpdatePassword} />
+                            <Button title="Close" onPress={this.handleOverlay} />
+                            {/* END OF OVERLAY */}
+
+
+                        </View>
+                    </View>
+                </Modal>
             </View>
         );
     }
@@ -183,7 +323,7 @@ class EditProfile extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor:'white',
+        backgroundColor: 'white',
         flex: 1,
         alignItems: 'center',
         // justifyContent: 'center',
@@ -226,8 +366,8 @@ const styles = StyleSheet.create({
         padding: 5,
         // width:'50%',
     },
-    btnTitle:{
-    fontSize: 16,
+    btnTitle: {
+        fontSize: 16,
 
     }
 });
