1dce79b6bdeadd537daf0c76f9c5217e155c91f2
Daniel Fisher <47793260+danielfisher414@users.noreply.github.com>
2023-05-01 12:03:30 +0100

    test

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -10,7 +10,7 @@ import Contacts from './screens/Contacts';
 const App = () => {
   const [id, setId] = useState(null);
   const [conversationId, conversationSetId] = useState(null);
-
+// tset
     // get user_id
   useEffect(() => {
     AsyncStorage.getItem('user_id')
